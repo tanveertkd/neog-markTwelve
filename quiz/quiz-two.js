5917385d6eb8b78b37cf293c229ddc46e2dc6118
tanveertkd <hazarikatanveer12@gmail.com>
2021-09-14 21:16:19 +0530

    fix - fixed bug with quiz two

--- quiz/quiz-two.js
+++ quiz/quiz-two.js
@@ -7,16 +7,33 @@ outputDiv.style.display = "none";
 const calculateHypotenuse = () => {
     const sideA = Number(userInputs[0].value);
     const sideB = Number(userInputs[1].value);
-    const sumOfSqSides = Math.pow(sideA, 2) + Math.pow(sideB, 2);
-    
-    const hypotenuse = Math.sqrt(sumOfSqSides).toFixed(3);
 
-    showOutput(hypotenuse);
+    const valid = validate(sideA, sideB);
+
+    if(valid){
+        const sumOfSqSides = Math.pow(sideA, 2) + Math.pow(sideB, 2);
+        const hypotenuse = Math.sqrt(sumOfSqSides).toFixed(3);
+        showOutput(hypotenuse, valid);
+    }else{
+        showOutput(null, valid);
+    }
 }
 
-const showOutput = (hypotenuse) => {
-    outputDiv.style.display = "block";
-    outputDiv.innerText =  "Hypotenuse is: "+hypotenuse;  
+const validate = (sideOne, sideTwo) => {
+    if(sideOne<0 || sideTwo<0){
+        return false;
+    }
+    return true;
+}
+
+const showOutput = (hypotenuse, valid) => {
+    if(valid){
+        outputDiv.style.display = "block";
+        outputDiv.innerText =  "Hypotenuse is: "+hypotenuse;
+    }else{
+        outputDiv.style.display = "block";
+        outputDiv.innerText = "Invalid input.";
+    }
 }
 
 btnSubmit.addEventListener('click', calculateHypotenuse);
